e86544628c1fc3c60ffea99c3bad451204f81ef4
chenaway <chenaway@qq.com>
2020-08-03 22:18:45 +0800

    solution for ex3

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,10 +36,14 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+temp = theta;
+temp(1) = 0;
 
+h = (e.^(-X*theta) + 1).^(-1);
 
+J = 1/m * sum(-y' * log(h) - (1-y)' *log(1-h)) + lambda/2/m * sum(temp.^2);
 
-
+grad = 1/m * (h-y)'*X + lambda/m*temp';
 
 
 
--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -29,8 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
+A = X * all_theta';
+[m,p] = max(A, [], 2);
 
 
 
